85d9162b52a8a321778990065f24a50166f7ec42
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2024-02-02 02:05:08 +0700

    fisrt-commit

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php 
+
+$server = "localhost";
+$user = "root";
+$password = "";
+$db = "diskusi_app";
+
+
+// Koneksi ke database
+$connect = new mysqli(
+    $server,
+    $user,
+    $password,
+    $db
+);
+
+
+
+?>
